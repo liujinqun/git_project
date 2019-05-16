4e565f72d0deb5115880c60e69b0406ecc42b8bb
liujinqun <306561523@qq.com>
2019-05-16 09:05:26 +0800

    提交到本地仓库

---
+++ student_prc/src/com/student/model/StudentModel.java
@@ -0,0 +1,19 @@
+package com.student.model;
+
+public class StudentModel {
+private int id;
+private String username;
+private String password;
+private int sex;
+private String love;
+
+public StudentModel(int id,String password,int sex,String love){
+	super();
+	this.id = id;
+	this.password = password;
+	this.username  = username;
+	this.sex = sex;
+	this.love =love;
+}
+
+}
